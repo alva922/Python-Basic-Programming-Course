1110bbd4f95efd3f622f68fd90d2df5ace4179a4
Al Va <102867612+alva922@users.noreply.github.com>
2024-03-19 19:23:02 +0100

    Create les1_get_strated.py
    Getting Started

---
+++ les1_get_strated.py
@@ -0,0 +1,13 @@
+#https://newdigitals.org/2024/01/23/basic-python-programming/#getting-started
+import os
+os.chdir('MYPATH')    # Set working directory YOURPATH
+os. getcwd()
+
+#check the Python version
+!python --version
+#Python 3.11.5
+#run the print script
+# Script Begins  
+print("I love Python!")  
+# Scripts Ends 
+#Output: I love Python!
